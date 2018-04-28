1b320c05a70396632b09950af1c0cd7c980b01fe
sayat23 <sayat23@mail.ru>
2018-04-28 14:14:36 +0600

    реализована сортировка задач во всем пунктам по имени пользователя, email и статусу

--- config/routes.php
+++ config/routes.php
@@ -1,21 +1,29 @@
 <?php
 
 return array(
-    'page-([0-9]+)'=>'site/index/$1',
+    //Создание задачи
     'task/create' => 'task/create',
 
-
+    //Сортировка данных в таблице с пагинацией
     'task/Sortbyusername/page-([0-9]+)' => 'task/Sortbyusername/$1',
+    'task/Sortbyemail/page-([0-9]+)' => 'task/Sortbyemail/$1',
+    'task/Sortbystatus/page-([0-9]+)' => 'task/Sortbystatus/$1',
+
+    //Сортировка данных в таблице
     'task/Sortbyusername' => 'task/Sortbyusername',
     'task/Sortbyemail' => 'task/Sortbyemail',
     'task/Sortbystatus' => 'task/Sortbystatus',
 
-
+    //Кабинет администратора
+    'cabinet/page-([0-9]+)' => 'cabinet/index/$1',
     'cabinet' => 'cabinet/index',
+
     // Пользователь:
     'user/login' => 'user/login',
     'user/logout' => 'user/logout',
+
     // Главная страница
+    'page-([0-9]+)'=>'site/index/$1',
     'index.php' => 'site/index/$1', // actionIndex в SiteController
     '' => 'site/index', // actionIndex в SiteController
 );
--- controllers/CabinetController.php
+++ controllers/CabinetController.php
@@ -10,14 +10,14 @@ class CabinetController
     /**
      * Action для страницы "Кабинет пользователя"
      */
-    public function actionIndex()
+    public function actionIndex($page=1)
     {
         // Получаем идентификатор пользователя из сессии
         $userId = User::checkLogged();
-        $tasks = tasks::getTasksList();
+        $tasks = tasks::getTasksList($page, $sort = 'username');
         // Получаем информацию о пользователе из БД
         $user = User::getUserById($userId);
-
+        $pagination = new Pagination(10,$page, tasks::SHOW_BY_DEFAULT, 'page-');
         // Подключаем вид
         require_once(ROOT . '/views/cabinet/index.php');
         return true;
